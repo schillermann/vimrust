8ba0f5b48c10fff5d453aee99061431ade758eed
Mario Schillermann <mario.schillermann@gmail.com>
2025-12-05 14:52:34 +0100

    welcome message
    Display editor name and version horizontally and vertically centered.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{io::{self, stdout, Write}, sync::Mutex, time::Duration};
+use std::{
+    io::{self, Write, stdout},
+    sync::Mutex,
+    time::Duration,
+};
 
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
@@ -11,6 +15,7 @@ use crossterm::{
 };
 
 static BUFFER: Mutex<Vec<u8>> = Mutex::new(Vec::new());
+static VERSION: &str = "0.1.0";
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
@@ -19,21 +24,38 @@ fn main() -> io::Result<()> {
     result
 }
 
-fn editor_draw_rows(buffer: &mut Vec<u8>, rows: u16) -> io::Result<()> {
-    for row in 0..rows {
-        queue!(buffer, MoveTo(0, row), Clear(ClearType::CurrentLine))?;
+fn editor_draw_rows(
+    buffer: &mut Vec<u8>,
+    number_of_columns: u16,
+    number_of_rows: u16,
+) -> io::Result<()> {
+    let welcome_row_number = number_of_rows / 3;
+    for row_number in 0..number_of_rows {
+        queue!(buffer, MoveTo(0, row_number), Clear(ClearType::CurrentLine))?;
         write!(buffer, "~")?;
+        if row_number == welcome_row_number {
+            let mut welcome = format!("VimRust -- version {}", VERSION);
+            let available_number_of_columns = number_of_columns - 1; // leave space for the leading tilde
+            if welcome.len() > available_number_of_columns as usize {
+                welcome.truncate(available_number_of_columns as usize);
+            }
+            let padding =
+                1 + (available_number_of_columns as usize / 2).saturating_sub(welcome.len() / 2);
+            let padding = padding.min(available_number_of_columns as usize);
+            queue!(buffer, MoveTo(padding as u16, row_number))?;
+            write!(buffer, "{}", welcome)?;
+        }
     }
     Ok(())
 }
 
 fn terminal_refresh(out: &mut io::Stdout) -> io::Result<()> {
-    let (_, rows) = size()?;
+    let (number_of_columns, number_of_rows) = size()?;
     let mut buffer = BUFFER.lock().unwrap();
     buffer.clear();
 
     queue!(&mut *buffer, Hide, MoveTo(0, 0))?;
-    editor_draw_rows(&mut buffer, rows)?;
+    editor_draw_rows(&mut buffer, number_of_columns, number_of_rows)?;
     queue!(&mut *buffer, MoveTo(0, 0), Show)?;
 
     out.write_all(&buffer)?;
