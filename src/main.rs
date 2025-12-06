96765c7f44f75677fd51682b0d071f048c47e62c
Mario Schillermann <mario.schillermann@gmail.com>
2025-12-06 22:10:54 +0100

    vertical scrolling
    Enable the user to scroll through the whole file, instead of just being able to see the top few lines of the file.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use crossterm::{
 static BUFFER: Mutex<Vec<u8>> = Mutex::new(Vec::new());
 static CURSOR_POSITION: Mutex<(u16, u16)> = Mutex::new((0, 0));
 static FILE_LINES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+static EDITOR_ROWS_OFFSET: Mutex<u16> = Mutex::new(0);
 static VERSION: &str = "0.1.0";
 
 fn main() -> io::Result<()> {
@@ -30,8 +31,9 @@ fn editor_draw_rows(
     buffer: &mut Vec<u8>,
     number_of_columns: u16,
     number_of_rows: u16,
+    rows_offset: u16,
+    file_lines: &Vec<String>,
 ) -> io::Result<()> {
-    let file_lines = FILE_LINES.lock().unwrap();
     let available_number_of_columns = number_of_columns.saturating_sub(1);
     if file_lines.is_empty() {
         let welcome_row_number = number_of_rows / 3;
@@ -53,7 +55,8 @@ fn editor_draw_rows(
     } else {
         for row_number in 0..number_of_rows {
             queue!(buffer, MoveTo(0, row_number), Clear(ClearType::CurrentLine))?;
-            if let Some(content) = file_lines.get(row_number as usize) {
+            let file_index = rows_offset as usize + row_number as usize;
+            if let Some(content) = file_lines.get(file_index) {
                 let mut line = content.clone();
                 if line.len() > available_number_of_columns as usize {
                     line.truncate(available_number_of_columns as usize);
@@ -67,27 +70,58 @@ fn editor_draw_rows(
     Ok(())
 }
 
-fn terminal_refresh(out: &mut io::Stdout, terminal_size: (u16, u16)) -> io::Result<()> {
+fn editor_scroll(cursor_index_y: u16, number_of_rows: u16, rows_offset: &mut u16) {
+    if cursor_index_y < *rows_offset {
+        *rows_offset = cursor_index_y;
+    }
+    if cursor_index_y >= number_of_rows {
+        *rows_offset = cursor_index_y
+            .saturating_sub(number_of_rows)
+            .saturating_add(1);
+    }
+}
+
+fn terminal_refresh(
+    out: &mut io::Stdout,
+    terminal_size: (u16, u16),
+    cursor_position: (u16, u16),
+    file_lines: &Vec<String>,
+) -> io::Result<()> {
     let (number_of_columns, number_of_rows) = terminal_size;
+    let mut rows_offset = EDITOR_ROWS_OFFSET.lock().unwrap();
+    editor_scroll(cursor_position.1, number_of_rows, &mut rows_offset);
     let mut buffer = BUFFER.lock().unwrap();
     buffer.clear();
-    let (cursor_index_x, cursor_index_y) = *CURSOR_POSITION.lock().unwrap();
 
     queue!(&mut *buffer, Hide, MoveTo(0, 0))?;
-    editor_draw_rows(&mut buffer, number_of_columns, number_of_rows)?;
-    queue!(&mut *buffer, MoveTo(cursor_index_x, cursor_index_y), Show)?;
+    editor_draw_rows(
+        &mut buffer,
+        number_of_columns,
+        number_of_rows,
+        *rows_offset,
+        file_lines,
+    )?;
+    queue!(
+        &mut *buffer,
+        MoveTo(cursor_position.0, cursor_position.1),
+        Show
+    )?;
 
     out.write_all(&buffer)?;
     out.flush()
 }
 
-fn editor_move_cursor(key_code: KeyCode, terminal_size: (u16, u16)) -> io::Result<()> {
-    let (number_of_columns, number_of_rows) = terminal_size;
-    let mut cursor_position = CURSOR_POSITION.lock().unwrap();
+fn editor_move_cursor(
+    key_code: KeyCode,
+    terminal_size: (u16, u16),
+    cursor_position: &mut (u16, u16),
+    file_lines: &Vec<String>,
+) -> io::Result<()> {
+    let (number_of_columns, _) = terminal_size;
     let (mut cursor_index_x, mut cursor_index_y) = *cursor_position;
 
     let cursor_index_x_max = number_of_columns.saturating_sub(1);
-    let cursor_index_y_max = number_of_rows.saturating_sub(1);
+    let cursor_index_y_max = (file_lines.len() as u16).saturating_sub(1);
 
     match key_code {
         KeyCode::Char('h') => {
@@ -112,7 +146,7 @@ fn editor_move_cursor(key_code: KeyCode, terminal_size: (u16, u16)) -> io::Resul
             }
         }
         KeyCode::Char('j') => {
-            if cursor_index_y < cursor_index_y_max {
+            if cursor_index_y < file_lines.len() as u16 {
                 cursor_index_y += 1;
             }
         }
@@ -129,8 +163,7 @@ fn editor_move_cursor(key_code: KeyCode, terminal_size: (u16, u16)) -> io::Resul
     Ok(())
 }
 
-fn editor_open() -> io::Result<()> {
-    let mut file_lines = FILE_LINES.lock().unwrap();
+fn editor_open(file_lines: &mut Vec<String>) -> io::Result<()> {
     file_lines.clear();
     file_lines.push("Hello, world!".to_string());
     Ok(())
@@ -140,17 +173,26 @@ fn run() -> io::Result<()> {
     let mut out = stdout();
     execute!(out, EnterAlternateScreen)?;
     let result: io::Result<()> = {
-        editor_open()?;
+        let mut file_lines = FILE_LINES.lock().unwrap();
+        editor_open(&mut file_lines)?;
+
         let mut terminal_size = size()?;
+        let mut cursor_position = CURSOR_POSITION.lock().unwrap();
+
         loop {
-            terminal_refresh(&mut out, terminal_size)?;
+            terminal_refresh(&mut out, terminal_size, *cursor_position, &file_lines)?;
             if event::poll(Duration::from_millis(50))? {
                 match event::read()? {
                     Event::Key(key_event) => {
                         if let KeyCode::Char('q') = key_event.code {
                             break;
                         } else {
-                            editor_move_cursor(key_event.code, terminal_size)?;
+                            editor_move_cursor(
+                                key_event.code,
+                                terminal_size,
+                                &mut cursor_position,
+                                &file_lines,
+                            )?;
                         }
                     }
                     Event::Resize(columns, rows) => {
